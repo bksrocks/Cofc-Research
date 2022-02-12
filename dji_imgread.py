e266b9f0a00b19e9ae06afb0c8b84e62141df2a8
Barbara Rocha <babih.123@gmail.com>
2022-02-12 14:19:34 -0500

    Updates on dji_imgread.py - data normalization.

--- dji_imgread.py
+++ dji_imgread.py
@@ -3,25 +3,41 @@ from matplotlib import pyplot as plt
 import earthpy.plot as ep
 import numpy as np
 
-# https://github.com/detecttechnologies/thermal_base
 
-dji_img_sample = "img/DJI_0354.JPG"
+def normalize_data(data):
+    # normalizes the data
+    return (data - np.min(data)) / (np.max(data) - np.min(data))
 
-# dji_img_sample =
 
-image = ThermalImage(image_path=dji_img_sample, camera_manufacturer="dji")
-thermal_np = image.thermal_np           # The temperature matrix as a np array
-raw_sensor_np = image.raw_sensor_np     # The raw thermal sensor excitation values as a np array
-meta = image.meta
+def read_image(image_path):
+    """This function transforms a DJI Mavic 2 Enterprise Due drone image into a numpy array.
+    input: JPG thermal image path.
+    output: temperature numpy array."""
 
-print(raw_sensor_np.shape)
+    # Uses the library thermal_base to process the DJI image.
+    # (https://github.com/detecttechnologies/thermal_base)
 
-fig, ax = plt.subplots(figsize=(10, 8))
-im = plt.imshow(thermal_np, interpolation='nearest')
-# plt.imshow(raw_sensor_np, interpolation='nearest')
-ep.colorbar(im)
-ax.set(title="12/14 - dji")
-ax.set_axis_off()
-plt.show()
+    image = ThermalImage(image_path=image_path, camera_manufacturer="dji")
 
-# np.savetxt("thermap.txt", thermal_np)
+    # thermal_np = image.thermal_np           # The temperature matrix as a np array
+    raw_sensor_np = image.raw_sensor_np     # The raw thermal sensor excitation values # as a np array
+    # meta = image.meta                     # Image metadata
+
+    return normalize_data(raw_sensor_np)
+
+
+def main():
+
+    # import and process drone image
+    dji_img_sample = read_image("img/DJI_0354.JPG")
+
+    # plot the image
+    fig, ax = plt.subplots(figsize=(10, 8))
+    im = plt.imshow(dji_img_sample, interpolation='nearest')
+    ep.colorbar(im)
+    ax.set(title="12/14 - dji")
+    ax.set_axis_off()
+    plt.show()
+
+
+main ()
