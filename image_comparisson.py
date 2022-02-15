02cefad21c925254c1a36831d06476dc2565c4eb
Barbara Rocha <babih.123@gmail.com>
2022-02-15 16:35:17 -0500

    Trying comparison method by histograms with OpenCV.

--- image_comparisson.py
+++ image_comparisson.py
@@ -8,89 +8,95 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 # from libxmp import XMPFiles, consts
 from skimage.metrics import structural_similarity as ssim
+import cv2
 
 
-def mse(imageA, imageB):
-    # the 'Mean Squared Error' between the two images is the
-    # sum of the squared difference between the two images;
-    # NOTE: the two images must have the same dimension
-    imageA = ma.resize(imageA, (imageB.shape[0], imageB.shape[1]))
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
+def compare_images(landsat, drone):
+    # image comparison utilizing https://www.pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
 
-    # return the MSE, the lower the error, the more "similar"
-    # the two images are
-    return err
+    # initialize the index dictionary to store the image name
+    # and corresponding histograms and the images dictionary
+    # to store the images themselves
+    index = {}
+    images = {}
 
+    # extract a 3D RGB color histogram from the image,
+    # using 8 bins per channel, normalize, and update
+    # the index
+    # print(type(landsat), landsat.depth(), type(drone), drone.depth())
+    hist1 = cv2.calcHist(landsat, [0], None, [256], [0, 256])
+    # hist1 = cv2.normalize(hist1, hist1).flatten()
 
-def compare_images(imageA, imageB, title):
-    # compute the mean squared error and structural similarity
-    # index for the images
-    m = mse(imageA, imageB)
-    s = ssim(ma.resize(imageA, (imageB.shape[0], imageB.shape[1])), imageB)
+    hist2 = cv2.calcHist(drone, [0], None, [256], [0, 256])
+    # hist2 = cv2.normalize(hist2, hist2).flatten()
 
-    # setup the figure
-    fig = plt.figure(title)
-    plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+    index[landsat] = hist1
+    index[drone] = hist2
 
-    # show first image
-    ax = fig.add_subplot(1, 2, 1)
-    plt.imshow(imageA, cmap = plt.cm.gray)
-    plt.axis("off")
+    OPENCV_METHODS = (("Correlation", cv2.HISTCMP_CORREL),
+                      ("Chi-Squared", cv2.HISTCMP_CHISQR),
+                      ("Intersection", cv2.HISTCMP_INTERSECT),
+                      ("Hellinger", cv2.HISTCMP_BHATTACHARYYA))
 
-    # show the second image
-    ax = fig.add_subplot(1, 2, 2)
-    plt.imshow(imageB, cmap = plt.cm.gray)
-    plt.axis("off")
+    # loop over the comparison methods
+    for (methodName, method) in OPENCV_METHODS:
+        # initialize the results dictionary and the sort
+        # direction
+        results = {}
+        reverse = False
 
-    # show the images
-    plt.show()
+        # if we are using the correlation or intersection
+        # method, then sort the results in reverse order
+        if methodName in ("Correlation", "Intersection"):
+            reverse = True
+
+        # loop over the index
+        for (k, hist) in index.items():
+            # compute the distance between the two histograms
+            # using the method and update the results dictionary
+            d = cv2.compareHist(index["doge.png"], hist, method)
+            results[k] = d
+
+        # sort the results
+        results = sorted([(v, k) for (k, v) in results.items()], reverse=reverse)
+
+        # show the query image
+        fig = plt.figure("Query")
+        ax = fig.add_subplot(1, 1, 1)
+        ax.imshow(images["doge.png"])
+        plt.axis("off")
+
+        # initialize the results figure
+        fig = plt.figure("Results: %s" % (methodName))
+        fig.suptitle(methodName, fontsize=20)
+
+        # loop over the results
+        for (i, (v, k)) in enumerate(results):
+            # show the result
+            ax = fig.add_subplot(1, len(images), i + 1)
+            plt.imshow(images[k])
+            plt.axis("off")
+
+        # show the OpenCV methods
+        plt.show()
 
 
 def main():
+    # Reading both images
     landsat_image = import_image("img/LC08_L1TP_016037_20211214_20211223_02_T1_B10.TIF", 10)
     dji_image, dji_meta = read_image("img/DJI_0354.JPG")
 
-    # path to the image or video
-    imagename = "img/DJI_0354.JPG"
+    # Getting GPS coordinates for dji drone image
+    imagepath = "img/DJI_0354.JPG"
 
-    with open(imagename, "rb") as fin:
+    with open(imagepath, "rb") as fin:
         img = fin.read()
         xmp_lat = str(img).find("GpsLatitude=")
         xmp_lon = str(img).find("GpsLongitude=")
         dji_lat = str(img)[xmp_lat+13:xmp_lat+24]
         dji_lon = str(img)[xmp_lon+14:xmp_lon+25]
 
-    # print(float(dji_lat), float(dji_lon))
-    #
-    # # lat_start = 33.176999 - float(dji_lat)
-    # # lon_start = -80.106913 - float(dji_lon)
-    #
-    # lat_pix_h = abs(34.123 - 33.825) / landsat_image.shape[0]
-    # lon_pix_h = abs(80.913 - 78.861) / landsat_image.shape[0]
-    #
-    # lat_pix_v = abs(34.123 - 32.518) / landsat_image.shape[1]
-    # lon_pix_v = abs(81.336 - 80.913) / landsat_image.shape[1]
-    #
-    # print(lat_pix_h, lon_pix_h)
-    # print(lat_pix_v, lon_pix_v)
-
-    # landsat_image_cut = landsat_image[lat_start:dji_image.shape[0]+lat_start, lon_start:dji_image.shape[1]+lon_start]
-
-    # print(lat_start)
-    # print(dji_image.shape[0]+lat_start)
-    # print(lon_start)
-    # print(dji_image.shape[1]+lon_start)
-
-    # plot the image
-    # fig, ax = plt.subplots(figsize=(10, 8))
-    # im = plt.imshow(landsat_image_cut, interpolation='nearest')
-    # ep.colorbar(im)
-    # ax.set(title="12/14")
-    # ax.set_axis_off()
-    # plt.show()
-
-    compare_images(landsat_image, dji_image, "12-14")
+    compare_images("img/LC08_L1TP_016037_20211214_20211223_02_T1_B10.TIF", "img/DJI_0354.JPG")
 
 
 main()
